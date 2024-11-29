4328b0e9e9360538e96d133a3a9cc0b732adff42
RajasriVeeramusti <165761211+RajasriVeeramusti@users.noreply.github.com>
2024-11-29 19:27:01 +0530

    Add files via upload

--- CalculateSum.java
+++ CalculateSum.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 public class CalculateSum{
  public static int CalculateSum(int a,int b){
- int sum=a+b;
- return sum;
+ return a+b;
  }
  public static void main(String args[])
  {
   Scanner sc=new Scanner(System.in);
   int a =sc.nextInt();
   int b=sc.nextInt();
-  int sum=CalculateSum(a,b);
-  System.out.println("The Sum of 2 numbers is: "+sum);
+ int sum = CalculateSum(a,b);
+  System.out.println("The sum of 2 numbers is: "+sum);
   }
   }
  
